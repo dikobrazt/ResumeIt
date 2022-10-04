eeefecb402eb262a6cd47b0464dcc9b8ddc321b3
Vladislav Tuleiko <>
2022-10-04 18:54:10 +0300

    space

--- ResumeIt/ResumeIt/ViewController.swift
+++ ResumeIt/ResumeIt/ViewController.swift
@@ -33,6 +33,11 @@ class ViewController: UIViewController {
             }
             
 
+            
+            
+            
+            
+            
         }
         // Do any additional setup after loading the view.
        
