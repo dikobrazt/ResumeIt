96a2a5a62f6949998f604f912739b089c90a924d
Vladislav Tuleiko <>
2022-10-04 18:19:39 +0300

    Task

--- ResumeIt/ResumeIt/ViewController.swift
+++ ResumeIt/ResumeIt/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var res: Int = 52
+    var i: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(res)
+        while res != 1 {
+            i = i+1
+            if res % 2 == 0{
+                res = res/2
+                print("\(res)")
+            } else {
+                res = 3*res + 1
+                print("\(res)")
+            }
+            if res == 1{
+                print("Кол-во итераций \(i)")
+            }
+            
+
+        }
         // Do any additional setup after loading the view.
+       
+       
     }
-
-
 }
 
